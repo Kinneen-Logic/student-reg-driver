4ce8ca36460d6cfb5122b83ea8a93520a59caee5
Aidan Kinneen <aidan.kinneen@gmail.com>
2018-12-11 02:13:35 +0000

    initial commit

---
+++ src/test/java/aidankinneen/StudentRegMavenDependancyTest/studentRegTestDependancy.java
@@ -0,0 +1,14 @@
+package aidankinneen.StudentRegMavenDependancyTest;
+
+import java.util.ArrayList;
+import org.joda.time.DateTime;
+
+
+public class studentRegTestDependancy {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+	}
+	
+}
